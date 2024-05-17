e64319bc90126751eb547d4002684e2ca7386414
Prem <premkumar@divami.com>
2024-05-17 22:31:34 +0530

    backend url updated

--- apps/web/app/contexts/SocketProvider.tsx
+++ apps/web/app/contexts/SocketProvider.tsx
@@ -34,7 +34,7 @@ const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
   }, []);
 
   useEffect(() => {
-    const _socket = io("http://localhost:8000");
+    const _socket = io("https://scalable-chat-app-b0b7.onrender.com");
     _socket.on("message", onMessageReceived);
     setSocket(_socket);
 
