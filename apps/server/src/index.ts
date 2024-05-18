4de3ff1b167e880e2e381bd3e4ce5413d4b0cea5
Prem <premkumar@divami.com>
2024-05-18 10:50:39 +0530

    health route added

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,22 @@
 import http from "http";
 import SocketService from "./services/socketService";
 const initiateServer = () => {
-  const app = http.createServer();
+  const app = http.createServer((req, res) => {
+    if (req.method === "GET" && req.url == "/health") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+
+      const healthCheck = {
+        uptime: process.uptime(),
+        message: "OK",
+        timestamp: Date.now(),
+      };
+
+      res.end(JSON.stringify(healthCheck));
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Not Found");
+    }
+  });
   const PORT = process.env.PORT ? process.env.PORT : 8001;
 
   const socketService = new SocketService();
